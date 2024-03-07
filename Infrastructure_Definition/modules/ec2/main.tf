254de08f264f8c0d6dd810c60b3943e32736e5f9
Naga Mallesh <89628161+Mallesh511@users.noreply.github.com>
2024-03-07 21:56:59 +0530

    Create main.tf

---
+++ Infrastructure_Definition/modules/ec2/main.tf
@@ -0,0 +1,25 @@
+# Fetching Ubuntu 20.04 AMI ID
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-**"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"]
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.instanceSize
+
+  tags = {
+    Name = var.stackName
+  }
+}
